042eb166ab25fe4a410ae5961025b92dc93839ba
rmoncorge <remim97@hotmail.fr>
2022-11-18 16:32:48 +0100

    Traitement Biss Vis

--- Extract_data.py
+++ Extract_data.py
@@ -1,18 +1,23 @@
 import csv
 import function as f
+from tkinter import filedialog as fd
+
 # path = "/Users/admin/OneDrive - Université de Bourgogne/Thèse/Bissolfzique/2021/Data brutes/Merge_all.csv"
+filename = fd.askopenfilename()
 # # path = "/Users/admin/Desktop/9030.csv"
-# all = []
+all = []
 #
-# with open(path, newline='') as csvfile:
-#     csv_reader = csv.reader(csvfile, delimiter=';', quotechar='|')
-#     header = next(csv_reader)
-#     header1 = next(csv_reader)
-#
-#     # Parcours du fichier
-#     for row in csv_reader:
-#         all.append(row)
-#
+with open(filename, newline='') as csvfile:
+    csv_reader = csv.reader(csvfile, delimiter=';', quotechar='|')
+    header = next(csv_reader)
+    header1 = next(csv_reader)
+
+    # Parcours du fichier
+    for row in csv_reader:
+        all.append(row)
+
+sujets = f.extract_long_court_visuel(all, filename)
+
 # # #tri_sujet_homme = f.find_male(all)
 #
 # bad_guys = f.mauvais_app(all)
@@ -22,13 +27,6 @@ import function as f
 # sujets = f.extract_long_court(tri_sujet_h_mauvais_app)
 
 # sujets = f.extract_long_court(all)
-
-path_windows = 'C:\Users\rmoncorge\OneDrive - Université de Bourgogne\Thèse\Bissolfzique\Data brutes\Merge_all.csv'
-all = []
-
-f = open(path, "r")
-for x in f:
-    all.append(x)
 
 
 
--- function.py
+++ function.py
@@ -312,4 +312,108 @@ def extract_long_court(all_trie):
     return liste_sujets
 
 
+# Même fonction pour sortir la proportion de long mais à pour les données de Bissolfzik visuel
+def extract_long_court_visuel(all_trie, filename):
+    final_list = []
+
+    # Parcours du fichier
+    for row in all_trie:
+        # Row
+        # 32 : Type sujet : 1 court Entree, 2 court Control
+        # 36 : 1 pour Biss1, 2 pour intermede, 3 pour Biss2
+        # 74 : 1 pour ENTER, 2 pour CONTROL
+        # 46 : procédure : APPRDUREE, TESTDUR, BISS1, BISS2
+        # 55 : durée stimulus
+
+        # On garde toutes les lignes avec les réponses longues et la ligne vide de l'intermède
+        if row[46] == 'BISS1' or row[46] == 'BISS2':
+            if row[32] != row[74]:  # on ne garde que les réponses longues
+                final_list.append(row)
+        #
+        # if row[36] == '2':  # et la ligne vide pour l'intermède
+        #     final_list.append(row)
+
+    # Récupérer la liste des numéros de sujets
+    liste_sujets = [1001]
+    last = 1001
+    compteur_sujet = 1
+
+    # Récupère tous les numéros des sujets
+    # Si le numéro de sujet est différent du dernier, je l'enregistre
+    for i in range(2, len(final_list)):
+        if int(final_list[i][1]) != int(final_list[i - 1][1]):
+            last = final_list[i][1]
+            liste_sujets.append(last)
+            compteur_sujet += 1
+    print('Nombre total de sujets : ', compteur_sujet)
+
+    # Comptage des durées
+    duree = [208, 272, 336, 400, 464, 528, 592]
+    head = ["Sujet", "Session", "Procedure", "DurStim", "RepLong", "RepCourt"]
+
+    # Première ligne du fichier final
+    with open(filename[:-14]+'Long_court.csv',
+              'w', newline='') as f:
+        writer = csv.writer(f)
+        writer.writerow(head)
+
+    j = 0  # variable pour les sujets
+    compt_duree2 = [0, 0, 0, 0, 0, 0, 0]  # compteur rep longue pour chaque durée
+    compt_duree1 = [0, 0, 0, 0, 0, 0, 0]  # bissection 1 / bissection 2
+
+    biss2 = False # Marqueur pour savoir si on est passé à la deuxième bissection
+
+    for i in range(1, len(final_list)):
+        if int(final_list[i][1]) == int(liste_sujets[j]):  # Si je suis encore au même sujet
+
+            # Si j'arrive à la deuxième bissection, j'enregistre les valeurs de la première
+            if final_list[i][46] == 'BISS2' and biss2 == False:
+                compt_duree1 = compt_duree2
+                compt_duree2 = [0, 0, 0, 0, 0, 0, 0]
+                biss2 = True
+
+            # Comptage
+            if final_list[i][55] == '208':
+                compt_duree2[0] += 1
+            if final_list[i][55] == '272':
+                compt_duree2[1] += 1
+            if final_list[i][55] == '336':
+                compt_duree2[2] += 1
+            if final_list[i][55] == '400':
+                compt_duree2[3] += 1
+            if final_list[i][55] == '464':
+                compt_duree2[4] += 1
+            if final_list[i][55] == '528':
+                compt_duree2[5] += 1
+            if final_list[i][55] == '592':
+                compt_duree2[6] += 1
+
+        # Si j'ai fini les données d'un sujet
+        else:
+            # print("End sujet", liste_sujets[j], compt_duree1, compt_duree2)
+            res = []  # tableau pour un sujet
+            # Je remplis le tableau avec les données sous la forme
+            # N°sujet N°Session Bissection 1/2 Durée NbLong NbCourt
+            for k in range(0, 14):
+                if k < 7:
+                    res.append(
+                        [final_list[i - 1][1], final_list[i - 1][2], 1, duree[k], compt_duree1[k], 10 - compt_duree1[k]])
+                else:
+                    res.append([final_list[i - 1][1], final_list[i - 1][2], 2, duree[k - 7], compt_duree2[k - 7],
+                                10 - compt_duree2[k - 7]])
+
+            # J'ajoute ces données à mon fichier
+            with open(filename[:-14]+'Long_court.csv','a', newline='') as f:
+                writer = csv.writer(f)
+                writer.writerows(res)
+
+            # Réinitialisation puis on passe au sujet suivant
+            j = j + 1
+            compt_duree2 = [0, 0, 0, 0, 0, 0, 0]
+            compt_duree1 = [0, 0, 0, 0, 0, 0, 0]
+            biss2 = False
+
+    return liste_sujets
+
+
 
